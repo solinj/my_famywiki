a04cc53144dccfb045d1626104e95cadad89363f
balech82 <ahmedhodroge02@gmail.com>
2018-10-31 10:33:43 +0100

    add tech

---
+++ tech/automatic_ssh.sh
@@ -0,0 +1,50 @@
+
+ssh-keygen -t rsa -b 4096 -C ahmedhodroge02@gmail.com
+
+#Flask version
+C:\Users\Fitec>flask --version
+Flask 1.0.2
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)]
+
+#Flask-Pymongo // PyMongo
+Successfully installed Flask-Pymongo-2.1.0 PyMongo-3.7.2
+
+"""C:\Users\Fitec>Flask_Pymongo --version
+'Flask_Pymongo' n’est pas reconnu en tant que commande interne
+ou externe, un programme exécutable ou un fichier de commandes."""
+
+# bson
+Successfully installed bson-0.5.7 python-dateutil-2.7.5 six-1.11.0
+
+"""C:\Users\Fitec>bson --version
+'bson' n’est pas reconnu en tant que commande interne
+ou externe, un programme exécutable ou un fichier de commandes."""
+
+#mango
+C:\Users\Fitec>mongo --version
+MongoDB shell version v4.0.3
+git version: 7ea530946fa7880364d88c8d8b6026bbc9ffa48c
+allocator: tcmalloc
+modules: none
+build environment:
+    distmod: 2008plus-ssl
+    distarch: x86_64
+    target_arch: x86_64
+	
+	
+C:\Users\Fitec>pip install mango
+Collecting mango
+  Could not find a version that satisfies the requirement mango (from versions: )
+No matching distribution found for mango
+You are using pip version 10.0.1, however version 18.1 is available.
+You should consider upgrading via the 'python -m pip install --upgrade pip' command.
+
+# datetime
+Successfully installed datetime-4.3 pytz-2018.7 zope.interface-4.6.0
+You are using pip version 10.0.1, however version 18.1 is available.
+You should consider upgrading via the 'python -m pip install --upgrade pip' command.
+
+
+"""C:\Users\Fitec>datetime --version
+'datetime' n’est pas reconnu en tant que commande interne
+ou externe, un programme exécutable ou un fichier de commandes."""
