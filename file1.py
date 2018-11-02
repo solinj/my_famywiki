047bf5c03c8de43503995ab0438f17af11f064a8
famywiki <margauxlaclare@gmail.com>
2018-11-02 16:45:06 +0100

    test

--- file1.py
+++
@@ -1,7 +0,0 @@
-# from jinja2 import Template
-# t = Template("Hello {{ something }}!")
-# t.render(something="World")
-#
-#
-# tt = Template("My favorite numbers: {% for n in range(1,10) %}{{n}} " "{% endfor %}")
-# tt.render()
