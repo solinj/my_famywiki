0bffd82ba51bcad7b34d6c15d7ad8808d1f46e9f
solinj <margauxlaclare@gmail.com>
2018-11-06 18:53:00 +0100

    fonction search

--- wiki.py
+++ wiki.py
@@ -15,20 +15,51 @@ import glob
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'wiki' # name of database on mongo
-app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/wiki"
+app.config['MONGO_DBNAME'] = 'Famywiki_Articles' # name of database on mongo
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/Famywiki_Articles"
 mongo = fp.PyMongo(app)
 
 
 
 @app.route("/article", methods=["GET"])
 def one_request():
-    print("SOMETHING")
-    # cursor = mongo.db.monuments.find({"REG":"Alsace"})
-    cursor = list(mongo.db.monuments.find({"REG":"Alsace"}))
-    print("after mongo")
 
+    # cursor = mongo.db.monuments.find({"REG":"Alsace"})
+    cursor = list(mongo.db.articles.find({"thème":"Autres"}))
     return render_template('template.html' , data = cursor)
+
+@app.route("/search/", methods=["GET", "POST"])
+def search():
+    if request.method == 'POST':
+        print(request.form['mot_cle'])
+        format_input = request.form['theme'].lower()
+        result_by_theme = list(mongo.db.articles.find({"thème":request.form['theme']}))
+        result_by_key_word = list(mongo.db.articles.find({"Titre":request.form['mot_cle']}))
+        return render_template('template.html', data = result_by_key_word)
+    else:
+        return render_template('HTML_BarreRecherche.html')
+    #, data=cursor
+
+
+
+
+
+# @app.route("/fiche/", methods=["GET", "POST"])
+# def fillFiche():
+#     source = ""
+#     if request.method == 'POST':
+#         if (fin.finalFunc(request.form['user_name'], request.form['user_prenom'])):
+#             source = "ok"
+#             return render_template('confirm.html')
+#         else:
+#                 # flash(u"rentrer donnees valides", 'error')
+#             source = "ko"
+#             return render_template('form_fp.html', src=source)
+#     return render_template('form_fp.html', src=source)
+
+
+
+
     # , data = cursor
     # return render_template('template.html', ip_address=(jsonify({'ip_address': request.remote_addr}), 200))
     # print((jsonify({'ip_address': request.remote_addr}), 200))
